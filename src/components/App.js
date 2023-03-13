898bbfec9358abc88b8a785631355fc0ac5eec44
amitkushwahatech <120050605+amitkushwahatech@users.noreply.github.com>
2023-03-13 20:22:42 +0530

    3rd solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React,{component,useState} from "react";
 import '../styles/App.css';
-const App = ()=>{
-  return (
-    <div id="root">
-      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-    </div>
-  )
-}
+// const App = () => {
+//   return (
+//     <div>
+//         <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+//     </div>
+//   )
+// }
+function App() {
+    return (
+      <div>
+        <p id='main'>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+      </div>
+    );
+  }
+  
+
 export default App;
